ef877652b7e7cfcc49bf9277b3ae8dcdf28a4e18
Abigail Watson <awatson1978@gmail.com>
2015-02-08 22:59:39 -0500

    need to export DowJones

--- package.js
+++ package.js
@@ -1,17 +1,18 @@
 Package.describe({
   summary: "Meteorite package that provides a simple history of the Dow Jones Industrial Average over 5 years.",
 
-    // update this value before you run 'meteor publish'
-    version: "1.0.1",
+  // update this value before you run 'meteor publish'
+  version: "1.0.2",
 
-    // if this value isn't set, meteor will default to the directory name
-    name: "awatson1978:dataset-dowjones",
+  // if this value isn't set, meteor will default to the directory name
+  name: "awatson1978:dataset-dowjones",
 
-    // and add this value if you want people to access your code from Atmosphere
-    git: "http://github.com/awatson1978/dataset-dowjones.git"
+  // and add this value if you want people to access your code from Atmosphere
+  git: "http://github.com/awatson1978/dataset-dowjones.git"
 });
 
 Package.on_use(function (api) {
-    api.use('standard-app-packages@1.0.4');
-    api.add_files('initialize.dowjones.js', ["client","server"]);
+  api.export('DowJones');
+  api.use('standard-app-packages@1.0.4');
+  api.addFiles('initialize.dowjones.js', ["client", "server"]);
 });
